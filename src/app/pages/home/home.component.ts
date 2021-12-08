8b4a0264fe1f42aa69a19f4564cf539d29708dcf
Evan <evan.w.mclaughlin@gmail.com>
2021-12-08 09:29:25 -0500

    Get ready for new doc site

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as Colors from '@pxblue/colors';
 import { ViewportService } from '../../services/viewport.service';
 
@@ -7,12 +7,52 @@ import { ViewportService } from '../../services/viewport.service';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     colors = Colors;
+
+    path: string;
+    remaining = 8;
+
+    // TODO: Update this to be the correct site after brightlayer-ui.github.io is published
+    target = 'brightlayer-ui-dev';
 
     constructor(private readonly _viewportService: ViewportService) {}
 
+    ngOnInit(): void {
+
+        this.path = window.location.pathname;
+        let params = window.location.href.split('?')[1];
+
+        if (params) {
+          params = '?' + params;
+          this.path = this.path + params;
+        }
+
+
+
+      console.log(this.path);
+        if (this.path === '/') {
+            this.path = '';
+        }
+
+        setInterval(() => {
+            this.remaining--;
+
+            if (this.remaining === 0) {
+                this.navigate();
+            }
+        }, 1000);
+    }
+
     isSmall(): boolean {
         return this._viewportService.isSmall();
+    }
+
+    navigate(): void {
+        window.location.href = `https://${this.target}.github.io${this.path}`;
+    }
+
+    learnMore(): void {
+        window.location.href = `https://brightlayer-ui.github.io/brightlayer-ui-rebrand`;
     }
 }
